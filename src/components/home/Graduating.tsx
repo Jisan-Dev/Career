d8f188c6aee060948aa08b3970571e966b2ab9da
Jisun696 <istiakkashem35@gmail.com>
2024-12-20 21:04:02 +0600

    ui update

--- src/components/home/Graduating.tsx
+++ src/components/home/Graduating.tsx
@@ -7,7 +7,7 @@ const Graduating = () => {
     <div className="container mx-auto my-32 flex items-center gap-32">
       <div className="bg-[#8C8AFF] w-[470px] h-[520px] rounded-lg flex items-center justify-center relative">
         <Image src={chele} height={578} alt="image" className="-mt-16" />
-        <Image src={s1} height={123} alt="image" className="absolute -top-[50] -right-[30]" />
+        <Image src={s1} height={123} alt="image" className="absolute -top-11 -right-10" />
       </div>
       <div>
         <h2 className="w-[570px] text-[52px] font-bold leading-[62px]">Just graduating? We can help.</h2>
