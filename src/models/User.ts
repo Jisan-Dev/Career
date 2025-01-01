d09b1d814341ff2a45ff8daff50e5f776d8709b7
Jisun696 <istiakkashem35@gmail.com>
2025-01-01 23:37:39 +0600

    create user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,9 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-export interface IUser {
+export interface IUser extends Document {
   email: string;
   password: string;
+  providerId: string;
   role: "candidate" | "recruiter" | "admin";
   profile: {
     name: string;
@@ -15,5 +16,37 @@ export interface IUser {
 
 // define mongoose schema for the User base on above interface
 const UserSchema: Schema<IUser> = new Schema({
-  email: { type: String, required: [true, "email is required"], unique: true },
+  email: { type: String, required: [true, "email is required"], trim: true, lowercase: true, unique: true },
+  password: {
+    type: String,
+    required() {
+      return !this.providerId;
+    },
+  },
+  providerId: {
+    type: String,
+    required() {
+      return !this.password;
+    },
+  },
+  role: { type: String, enum: ["candidate", "recruiter", "admin"], default: "candidate" },
+  profile: {
+    name: { type: String, required: true },
+    image: { type: String },
+    phone: { type: String },
+    company: {
+      type: String,
+      required: function () {
+        return this.role === "recruiter";
+      },
+    },
+    position: {
+      type: String,
+      required: function () {
+        return this.role === "recruiter";
+      },
+    },
+  },
 });
+
+export const User = (mongoose.models.User as mongoose.Model<IUser>) || mongoose.model<IUser>("User", UserSchema);
