f6a3cf8919376231269096ed16ae791378937900
Jisun696 <istiakkashem35@gmail.com>
2025-01-04 23:14:27 +0600

    Hash the password before saving it to the database

--- src/app/api/create-user/route.ts
+++ src/app/api/create-user/route.ts
@@ -1,29 +1,22 @@
 import dbConnect from "@/lib/dbConnect";
 import { User } from "@/models/User";
+import { hash } from "bcryptjs";
 
 export async function POST(request: Request) {
   await dbConnect();
   try {
     const receivedData = await request.json();
-    console.log(receivedData);
     if (receivedData.firstName && receivedData.lastName) {
       receivedData.username = receivedData.firstName + " " + receivedData.lastName;
       delete receivedData.firstName;
       delete receivedData.lastName;
     }
 
-    // const newUser = new User({
-    //   email: receivedData.email,
-    //   password: receivedData.password,
-    //   role: receivedData.role || "candidate",
-    //   profile: {
-    //     name: receivedData.firstName + " " + receivedData.lastName,
-    //     image: receivedData.image || "",
-    //     phone: receivedData.phone || "",
-    //     company: receivedData.company || "",
-    //     position: receivedData.position || "",
-    //   },
-    // });
+    // Hash the password before saving it to the database
+    // const hashedPassword = await User.hashPassword(receivedData.password); // This is a static method in the User model
+    // newUser.password = hashedPassword
+    const hashedPassword = await hash(receivedData.password, 10);
+    receivedData.password = hashedPassword;
     const newUser = new User(receivedData);
     await newUser.save();
     return Response.json({ success: true, message: "User registered successfully" });
